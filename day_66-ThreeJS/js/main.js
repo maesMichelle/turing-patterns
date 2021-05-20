8ad58665d3716f64d13ee8003078df9c7c24d6c7
maesMichelle <michelle.maes@student.kdg.be>
2021-05-20 11:19:36 +0200

    update

---
+++ day_66-ThreeJS/js/main.js
@@ -0,0 +1,73 @@
+import * as THREE from "/third_party/three.module.js";
+import * as dat from "/third_party/dat.gui.module.js";
+
+import ColorMapLayer from "./layers/color-map.js";
+import DifferenceLayer from "./layers/difference.js";
+import ImageLayer from "./layers/image.js";
+import ReactionDiffusionLayer from "./layers/reaction-diffusion.js";
+import WebcamLayer from "./layers/webcam.js";
+import MirrorLayer from "./layers/mirror.js";
+
+let canvas, camera, renderer, layers, mesh, material;
+
+async function main() {
+  canvas = document.getElementById("c");
+  let width = canvas.width;
+  let height = canvas.height;
+
+  const gui = new dat.GUI();
+  window.gui = gui;
+
+  camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+  renderer = new THREE.WebGLRenderer({
+    canvas,
+    preserveDrawingBuffer: true,
+    powerPreference: "high-performance",
+    depth: false,
+    stencil: false,
+  });
+  renderer.autoClear = false;
+  renderer.setSize(width, height);
+
+  const geometry = new THREE.PlaneGeometry(2, 2);
+  material = new THREE.MeshBasicMaterial({ color: "white" });
+  mesh = new THREE.Mesh(geometry, material);
+
+  const imageLayer = new ImageLayer("./img/bloem.jpg");
+  const colorMapLayer = new ColorMapLayer("./img/vis.png");
+  const reactionDiffusionLayer = new ReactionDiffusionLayer();
+  const webcamLayer = new WebcamLayer();
+  const differenceLayer = new DifferenceLayer();
+  const mirrorLayer = new MirrorLayer();
+
+  layers = [webcamLayer, mirrorLayer, differenceLayer, reactionDiffusionLayer, colorMapLayer];
+  //layers = [webcamLayer, differenceLayer];
+  //layers = [webcamLayer, differenceLayer, reactionDiffusionLayer];
+  //layers = [webcamLayer, differenceLayer, reactionDiffusionLayer, colorMapLayer];
+  //layers = [imageLayer, reactionDiffusionLayer];
+  //layers = [webcamLayer, colorMapLayer];
+
+  for (const layer of layers) {
+    await layer.setup(width, height);
+  }
+
+  requestAnimationFrame(animate);
+}
+
+function animate(elapsedTime) {
+  for (let i = 0; i < layers.length; i++) {
+    layers[i].draw(renderer, camera, elapsedTime, layers[i - 1]);
+  }
+
+  let lastLayer = layers[layers.length - 1];
+  material.map = lastLayer.target.texture;
+
+  renderer.setRenderTarget(null);
+  renderer.render(mesh, camera);
+
+  requestAnimationFrame(animate);
+}
+
+main();
+// window.addEventListener("click", animate);
